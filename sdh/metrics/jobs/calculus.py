ff5181db64b2c9dcc7c0457e8e40d45810b122a3
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-07-15 10:41:42 +0200

    Refactor and global parameters

--- sdh/metrics/jobs/calculus.py
+++ sdh/metrics/jobs/calculus.py
@@ -25,21 +25,26 @@
 __author__ = 'Fernando Serena'
 
 import calendar
-from datetime import datetime, date, timedelta
+from datetime import date
 from threading import Thread
 from datetime import datetime, timedelta
-import pytz
 from rdflib import Literal
+import multiprocessing
 
 __calculus = set([])
 __dates = set([])
 __triggers = {}
+
+workers = multiprocessing.cpu_count()
+MAX_ACUM_DATES = workers * 100
+
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     if n:
         for i in xrange(0, len(l), n):
             yield l[i:i + n]
+
 
 def add_calculus(func, triggers):
     __calculus.add(func)
@@ -49,38 +54,27 @@ def add_calculus(func, triggers):
                 __triggers[trigger] = set([])
             __triggers[trigger].add(func)
 
-def make_calculus(calculus, t_begin, t_end):
-    # pre = datetime.now()
-    calculus(t_begin, t_end)
-    # print calculus.func_name, t_begin, t_end, (datetime.now() - pre).microseconds
 
-def reset_time_interval():
-    return pytz.utc.localize(datetime.utcnow()), pytz.utc.localize(datetime.utcfromtimestamp(0))
-
-
-def check_triggers(collector, (t, s, p, o), stop_event, app):
-    # min_date, max_date = reset_time_interval()
-
+def check_triggers(collector, (t, s, p, o), stop_event):
     if collector in __triggers:
         if isinstance(o, Literal):
             obj = o.toPython()
             if isinstance(obj, datetime):
                 __dates.add(date(obj.year, obj.month, obj.day))
-                if len(__dates) > 100:
-                    date_chunks = chunks(list(__dates), 10)
+                if len(__dates) >= MAX_ACUM_DATES:
+                    date_chunks = chunks(list(__dates), workers)
                     for chunk in date_chunks:
                         threads = []
                         for d in chunk:
-                            thread = Thread(target=calculate_metrics, args=(d, d, stop_event, app, __triggers[collector]))
+                            thread = Thread(target=calculate_metrics,
+                                            args=(d, d, stop_event, __triggers[collector]))
                             threads.append(thread)
                             thread.start()
                         [t.join() for t in threads]
                     __dates.clear()
 
 
-def calculate_metrics(first_date, max_date, stop_event, app, calcs):
-    app.store.execute_pending()
-
+def calculate_metrics(first_date, max_date, stop_event, calcs):
     next_date = date(first_date.year, first_date.month, first_date.day)
     max_date = date(max_date.year, max_date.month, max_date.day) + timedelta(days=1)
     print 'Making calculus for [{}, {}]...'.format(next_date, max_date),
@@ -93,10 +87,9 @@ def calculate_metrics(first_date, max_date, stop_event, app, calcs):
 
         # Run all registered calculus
         for c in calcs:
-            make_calculus(c, t_begin, t_end)
+            c(t_begin, t_end)
 
         next_date = next_date + timedelta(days=1)
         if stop_event.isSet():
             return
     print 'it took {}ms'.format((datetime.now() - pre).microseconds / float(1000))
-    # app.store.execute_pending()
