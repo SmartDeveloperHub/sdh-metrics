53c0e60a81c6015a02debd4c2d3dc6cf4df0ab5b
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-07-16 11:52:16 +0200

    Added aggregators to store module

--- sdh/metrics/store/__init__.py
+++ sdh/metrics/store/__init__.py
@@ -24,9 +24,82 @@
 
 __author__ = 'Fernando Serena'
 
+import calendar
+from datetime import date
+import types
+
 import pkg_resources
+
 try:
     pkg_resources.declare_namespace(__name__)
 except ImportError:
     import pkgutil
+
     __path__ = pkgutil.extend_path(__path__, __name__)
+
+
+def __build_time_chunk(store, key, begin, end):
+    _next = begin
+    while _next < end:
+        _end = _next + 86400
+        stored_values = [eval(res)['v'] for res in store.db.zrangebyscore(key, _next, _end - 1)]
+        if stored_values:
+            for v in stored_values:
+                yield v
+        else:
+            yield 0
+        _next = _end
+
+
+def aggregate(store, key, begin, end, num, aggr=sum):
+    if begin is None:
+        end_limit = end
+        if end is None:
+            end_limit = '+inf'
+        _, begin = store.db.zrangebyscore(key, '-inf', end_limit, withscores=True, start=0, num=1).pop()
+        data_begin = begin
+    else:
+        _, data_begin = store.db.zrangebyscore(key, '-inf', '+inf', withscores=True, start=0, num=1).pop()
+    if end is None:
+        _, end = store.db.zrevrangebyscore(key, '+inf', begin, withscores=True, start=0, num=1).pop()
+        data_end = end
+    else:
+        _, data_end = store.db.zrevrangebyscore(key, '+inf', '-inf', withscores=True, start=0, num=1).pop()
+
+    begin = calendar.timegm(date.fromtimestamp(begin).timetuple())
+    end = calendar.timegm(date.fromtimestamp(end).timetuple())
+
+    step_begin = begin
+    values = []
+
+    step = end - begin
+    if num:
+        step /= num
+    step = max(86400, step)
+
+    while step_begin <= end - step:
+        step_end = step_begin + step
+        if aggr == sum and num:
+            chunk = [eval(res)['v'] for res in store.db.zrangebyscore(key, step_begin, step_end)]
+        else:
+            chunk = __build_time_chunk(store, key, step_begin, step_end)
+        values.append(chunk)
+        step_begin = step_end
+
+    if num:
+        result = [aggr(part) for part in values]
+    else:
+        result = list(values.pop())
+        if any(isinstance(el, list) for el in result):
+            result = [len(x) for x in result]
+
+    return {'begin': begin, 'end': end, 'data_begin': data_begin, 'data_end': data_end, 'step': step}, result
+
+
+def avg(x):
+    if isinstance(x, types.GeneratorType):
+        x = list(x)
+    if type(x) == list:
+        if x:
+            return sum(x) / float(len(x))
+    return 0
